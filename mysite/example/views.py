c762f30d278fe01b9e13118ce544c46a8cf0d643
GabeatUNSW <36873727+GabeatUNSW@users.noreply.github.com>
2019-06-22 14:09:30 +1000

    1

--- mysite/example/views.py
+++ mysite/example/views.py
@@ -3,6 +3,7 @@ from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 import json
 from .models import *
+import os, sys
 
 # Create your views here.
 
@@ -21,11 +22,30 @@ def example_get(request, var_a, var_b):
 
 @csrf_exempt
 def example_post(request):
+	jsob = {"starNumber": 0, "length": 10}
 	log = []
 	if request.method == "POST":
 		try:
 			data = request.POST["data"]
-			return JsonResponse({"log":log})
+			received = json.loads(data)
+
+			###########################
+			##custom function below####
+			###########################
+			starNumber = int(son ["starNumber"])
+			length = int (jsob ["length"])
+			loop = range(length)
+
+			numarray = []
+
+			fibno = starNumber
+			addno = 1
+			for l in loop:
+				numarray.append(fibno)
+				fibno = fibno + addno
+				addno = fibno - addno
+
+			return JsonResponse({"fib":numarray})
 		except Exception as e:
 			exc_type, exc_obj, exc_tb = sys.exc_info()
 			other = sys.exc_info()[0].__name__
@@ -34,3 +54,39 @@ def example_post(request):
 			return JsonResponse({"isError": True, "error":str(e), "errorType":errorType, "function":fname, "line":exc_tb.tb_lineno, "log":log})
 	else:
 		return HttpResponse("ONLY POST REQUESTS")
+
+@csrf_exempt
+def fib(request):
+	jsob = {"starNumber": 0, "length": 10}
+	log = []
+	if request.method == "POST":
+		try:
+			data = request.POST["data"]
+			received = json.loads(data)
+
+			###########################
+			##custom function below####
+			###########################
+			starNumber = int(jsob ["starNumber"])
+			length = int (jsob ["length"])
+			loop = range(length)
+
+			numarray = []
+
+			fibno = starNumber
+			addno = 1
+			for l in loop:
+				numarray.append(fibno)
+				fibno = fibno + addno
+				addno = fibno - addno
+
+			return JsonResponse({"fib":numarray})
+		except Exception as e:
+			exc_type, exc_obj, exc_tb = sys.exc_info()
+			other = sys.exc_info()[0].__name__
+			fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+			errorType = str(exc_type)
+			return JsonResponse({"isError": True, "error":str(e), "errorType":errorType, "function":fname, "line":exc_tb.tb_lineno, "log":log})
+	else:
+		return HttpResponse("ONLY POST REQUESTS")
+
